3e5cae1389bfd0ca65881165c7fdec04031770d4
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-09-12 10:25:18 +0700

    demo project done

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-main>
+      <Navbar />
+
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+
+  components: {
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
